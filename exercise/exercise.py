7a63ad6709cfd3b869ab9a77019766633b401f92
Blake Crosby <ptsouth97@gmail.com>
2018-05-17 22:49:43 -0400

    updated to only select rows where exercise is running

--- exercise/exercise.py
+++ exercise/exercise.py
@@ -3,16 +3,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data_file = 'com.samsung.health.exercise.201804281858.xlsx'
 
-df = pd.read_excel(data_file, skiprows=1)
+def main():
+	'''main loop'''
 
-x = df['start_time']
-y = df['mean_speed']
+	# read exercise file into a dataframe
+	data_file = 'com.samsung.health.exercise.201804281858.xlsx'
+	df = pd.read_excel(data_file, skiprows=1)
 
-plt.plot(x, y, linestyle='none', marker='.')
-plt.xlabel('Date')
-plt.ylabel('Mean speed')
-plt.xticks(rotation='vertical')
-plt.show()
+	# select rows where the exercise code indicates running
+	running = df.loc[df['exercise_type'] == 1002]
 
+	# slice the columns with the date and average speed
+	x = running['start_time']
+	y = running['mean_speed']
+
+	# make a plot
+	plt.plot(x, y, linestyle='none', marker='.')
+	plt.xlabel('Date')
+	plt.ylabel('Mean speed')
+	plt.xticks(rotation='vertical')
+	plt.show()
+
+
+if __name__ == '__main__':
+	main()
