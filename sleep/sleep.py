7f1d9eb8c42aa1ff85da570814c30b0b92d5d44f
ptsouth97 <ptsouth97@gmail.com>
2019-03-07 15:39:29 -0500

    working on sleep graph

--- sleep/sleep.py
+++ sleep/sleep.py
@@ -1,13 +1,56 @@
 #!/usr/bin/python3
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
 
-data_file = 'com.samsung.health.sleep.201805102139.csv'
 
-df = pd.read_csv(data_file, skiprows=1)
+def main():
+	''' Main function for testing purposes'''
 
-end_time = pd.to_datetime(df.iloc[208][7])
-start_time = pd.to_datetime(df.iloc[208][2])
-duration = end_time - start_time
+	data_file = 'com.samsung.health.sleep.201903071432.csv'
 
-print('You slept for {}'.format(duration))
+	# Read in the data file
+	df = pd.read_csv(data_file, skiprows=1)
+
+	plot_hours(df)
+
+	'''
+	end_time = pd.to_datetime(df.iloc[208][7])
+	start_time = pd.to_datetime(df.iloc[208][2])
+	duration = end_time - start_time
+
+	print('You slept for {}'.format(duration))
+	'''
+
+
+def plot_hours(df):
+	''' Plots hours slept each night'''
+
+	# add a duration column
+	df['duration'] = (pd.to_datetime(df['end_time']) - pd.to_datetime(df['start_time']))#.dt.components['seconds'] #.astype('timedelta64[h]')
+
+	# filter out hours above 12 as erroneous
+	#df = df[df['duration']<12]
+
+	# slice the start time column and keep the date, but not the time
+	x = df['start_time'].str.split(' ').str[0]
+	
+	y = df['duration']#.dt.datetime.time()
+
+	print(type(y[0]))
+	# make a plot
+	plt.plot(x, y, linestyle='none', marker='.')
+	plt.title('Sleep duration')
+	plt.xlabel('Date')
+	plt.ylabel('Duration (hours)')
+	plt.xticks(rotation='vertical')
+	plt.tight_layout()
+	plt.savefig('sleep.jpg')
+	plt.show()
+	
+	return
+
+
+if __name__ == '__main__':
+	main()
